9613d187b573ccc088957212c35033263f2385b4
TimothyAEkawu <ekawuachichy@gmail.com>
2022-07-01 17:55:27 -0700

    6 printing numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - display 0 - 9
+ * Return: 0 if there is no hikky
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
