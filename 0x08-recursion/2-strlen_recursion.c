e088714dfb9557b3383c2f32254c9d83fb8f1a6e
TimothyAEkawu <ekawuachichy@gmail.com>
2022-07-21 02:46:50 -0700

    strlen recursion C

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - get the number of character in a string
+ * @s: pointer to the first character of the string
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
