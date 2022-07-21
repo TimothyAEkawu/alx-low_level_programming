7a15e09c212ef74eb31800f7f4f828c9f87fa16c
TimothyAEkawu <ekawuachichy@gmail.com>
2022-07-21 02:38:56 -0700

    rev recursion C

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: pointer to the first character of the string
+ *
+ * Return: nothing or void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
